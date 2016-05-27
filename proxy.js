909d9b3b2eb6f5e87551e1b27140b39829d87c71
jfo <jfojfo@gmail.com>
2016-05-27 18:16:09 +0800

    Revert "update app key"
    This reverts commit 36bb901c1920a7cee6450ba2490f6b14092f688d.

--- proxy.js
+++ proxy.js
@@ -117,15 +117,6 @@ var options = {
 console.log('Listening at 0.0.0.0:' + HTTPS_PORT);
 console.log('Listening at 0.0.0.0:' + HTTP_PORT);
 
-
-var AV = require('leanengine');
-
-var APP_ID = process.env.LC_APP_ID;
-var APP_KEY = process.env.LC_APP_KEY;
-var MASTER_KEY = process.env.LC_APP_MASTER_KEY;
-
-AV.initialize(APP_ID, APP_KEY, MASTER_KEY);
-
 var PORT = parseInt(process.env.LC_APP_PORT || 3000);
 http.createServer()
     .on('request', request)
